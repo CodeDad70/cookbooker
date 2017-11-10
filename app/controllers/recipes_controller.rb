29e05bc7d3a96989524c1af1699f0d2ab89d0c2d
CodeDad <github email address>
2017-11-09 20:32:59 -0500

    made a route to call up the creaet recipe form. also created a link to the create recipe form on the recipes index page so once they user logs in they can jump over

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,12 +3,23 @@ class RecipesController < ApplicationController
   # GET: /recipes
   
   get '/recipes' do
-  	if logged_in?     
+  	@user = current_user
+  	if logged_in?    
    	 erb :'/recipes/index'
   	else
   		redirect to "/users/login"
   	end
   end
+
+  get '/recipes/new' do
+  	if logged_in? 
+  		erb :'recipes/create_recipe'
+  	else
+  		redirect to "/users/login"
+  	end
+  end
+
+ 
 
   
 end
